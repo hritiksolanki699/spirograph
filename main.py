4395bfbbe4c2a8ee2f357d75e19b36651d8e43c1
Hritik <hritiksolanki699@gmail.com>
2022-05-28 11:33:49 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+import turtle as t
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+t.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
+
+
+timmy.speed("fastest")
+
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        timmy.color(random_color())
+        timmy.circle(100)
+        timmy.setheading(timmy.heading() + size_of_gap)
+
+
+draw_spirograph(3)
+
+my_screen = Screen()
+my_screen.exitonclick()
